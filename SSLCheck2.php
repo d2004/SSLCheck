16c4e06156e0303e2d623696728f5f8458b76c25
Davide Ramondetti <davideramondetti@yahoo.it>
2016-09-25 21:40:00 +0200

    Update SSLCheck2.php

--- SSLCheck2.php
+++ SSLCheck2.php
@@ -1,1 +1,7 @@
 <?php
+if (!isset($_SERVER['HTTPS']) || !$_SERVER['HTTPS']) {
+   exit("<h1>YOUR CONNECTION IS NOT SECURE! ACCESS DENIED!</h1>Excuse me, but your connection is not secure and it cannot connect via HTTPS. Please disable sslstrip and activate HTTPS connections or you cannot join! (it is possible you are under a ManInTheMiddle Attack. )");
+} else {
+  header("Location: ".$_COOKIE['sslcheck_redir']);
+}
+?>
